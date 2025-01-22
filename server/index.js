963d6d49db2fb4b159bc817ac2c66eab885355d2
ALRAZOUQ <rbmale3nk@gmail.com>
2025-01-22 17:17:57 +0300

    setting up ...

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
+// Libraries imports
 import express from "express"
+
+
+
+// routers import:
+import coursesRouter from "./routers/courses_page.js"
+//  =====================
 
 const app = express()
 const port = 5000
+
+
+app.use(coursesRouter)
+app.get("/", (req, res) => {
+    res.json("Home page :) ")
+})
 
 
 app.listen(port, () => console.log(`Server listen to the port ${port}`))
