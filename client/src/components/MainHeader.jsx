cd123506ca2931964019c876e7bc38c2fcb0d98c
Faisal4z <443102216@student.ksu.edu.sa>
2025-02-04 22:42:20 +0300

    add loggin and logout buttons to the header and some links but as comment until now

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect ,useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X, Search } from "lucide-react";
 import main_logo from "../assets/mainLogo.svg";
@@ -7,39 +7,45 @@ import { useCourseData } from "../context/CourseContext";
 export default function MainHeader() {
   const [searchInput, setSearchInput] = useState("");
   const [isOpen, setIsOpen] = useState(false);
- 
+const [isLogged, setIsLogged] = useState(false);//we have to update the state bases on userContext
   const [filterdCourses, setfilterdCourses] = useState(null);
   const [showResults, setShowResults] = useState(false);
-// Use the context to get courses data
-const { coursesData, setCoursesData } = useCourseData();
+  // Use the context to get courses data
+  const { coursesData, setCoursesData } = useCourseData();
 
-// Only fetch the data one time then use 
-useEffect(() => {
-  if (!coursesData) {
-    axios.get('auth/coursesTiteles')
-      .then((response) => {
-        const coursesArray = Array.isArray(response.data) ? response.data : [];
-       // console.log(response.data);
-        setCoursesData(coursesArray);
-        setfilterdCourses(coursesArray);
-      })
-      .catch((error) => {
-        console.error("API Error:", error);
-      });
-  }
-}, [coursesData, setCoursesData]);
+  // Only fetch the data one time then use
+  useEffect(() => {
+    if (!coursesData) {
+      axios
+        .get("auth/coursesTiteles")
+        .then((response) => {
+          const coursesArray = Array.isArray(response.data)
+            ? response.data
+            : [];
+          // console.log(response.data);
+          setCoursesData(coursesArray);
+          setfilterdCourses(coursesArray);
+        })
+        .catch((error) => {
+          console.error("API Error:", error);
+        });
+    }
+  }, [coursesData, setCoursesData]);
 
-/*note if the admin create new course we have to update the data stored in the context CourseContext */
+  /*note if the admin create new course we have to update the data stored in the context CourseContext */
   const onSearch = (e) => {
     const { value } = e.target;
     setSearchInput(value);
     setShowResults(value.length > 0);
 
     if (coursesData) {
-      setfilterdCourses(coursesData.filter((course) =>
-      course.code.toLowerCase().includes(value.toLowerCase())||
-        course.name.toLowerCase().includes(value.toLowerCase())
-      ));
+      setfilterdCourses(
+        coursesData.filter(
+          (course) =>
+            course.code.toLowerCase().includes(value.toLowerCase()) ||
+            course.name.toLowerCase().includes(value.toLowerCase())
+        )
+      );
     }
   };
 
@@ -54,17 +60,20 @@ useEffect(() => {
             alt="TAFAWOUQ LOGO"
           />
         </h1>
+
+        
         <button
           className="md:hidden text-white focus:outline-none"
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? <X size={28} /> : <Menu size={28} />}
         </button>
+        
       </div>
 
       {/* Navigation Links & Search Bar */}
       <div
-        className={`w-full md:flex md:items-center md:gap-8 ${
+        className={`w-full md:flex md:items-center md:gap-8  ${
           isOpen ? "flex flex-col gap-4" : "hidden"
         } md:flex-row md:justify-center`}
       >
@@ -88,17 +97,26 @@ useEffect(() => {
             <div className="absolute z-50 w-full mt-1 bg-white rounded-md border border-gray-300 shadow-lg">
               <div className="max-h-60 overflow-y-auto">
                 {filterdCourses?.length === 0 ? (
-                  <div className="p-3 text-gray-500 font-cairo">لا توجد نتائج</div>
+                  <div className="p-3 text-gray-500 font-cairo">
+                    لا توجد نتائج
+                  </div>
                 ) : (
                   <ul className="py-2">
                     {filterdCourses?.map((course) => (
-                      <li key={course.id} className="px-3 py-2 hover:bg-gray-100 group/searchResult">
+                      <li
+                        key={course.id}
+                        className="px-3 py-2 hover:bg-gray-100 group/searchResult"
+                      >
                         <Link
                           to={`/course/${course.id}`}
                           className="block font-cairo text-lg  "
                         >
-                          <p className="text-gray-700 group-hover/searchResult:text-blue-500 ">{course.name} | <span className="font-bold text-gray-900">{course.code}</span></p>
-                         
+                          <p className="text-gray-700 group-hover/searchResult:text-blue-500 ">
+                            {course.name} |{" "}
+                            <span className="font-bold text-gray-900">
+                              {course.code}
+                            </span>
+                          </p>
                         </Link>
                       </li>
                     ))}
@@ -108,7 +126,41 @@ useEffect(() => {
             </div>
           )}
         </div>
+
+
+  {/* Additional Links */}
+   {/*
+  <div className="flex flex-col md:flex-row gap-2 md:gap-8 mt-4 md:mt-0 justify-center items-center ">
+  <Link to="/" className="text-white hover:text-gray-300 transition-colors w-full md:w-auto text-center p-1 md:p-0 font-cairo">
+       القائمة الرئيسية
+  </Link>
+  <Link to="/about" className="text-white hover:text-gray-300 transition-colors w-full md:w-auto text-center p-1 md:p-0 font-cairo">
+    حول
+  </Link>
+  <Link to="/contact" className="text-white hover:text-gray-300 transition-colors w-full md:w-auto text-center p-1 md:p-0 font-cairo">
+    التواصل
+  </Link>
+  </div>
+  */}
+
+  {/* Sign Up Button mobile */}
+
+ 
+  {isLogged ?(  <button className=" md:hidden bg-TAF-100 text-white px-4 py-2 rounded-md hover:opacity-75 active:opacity-50 transition-colors font-cairo  ">
+     سجل الخروج
+        </button>):(
+      <button className="md:hidden  bg-TAF-100 text-white px-4 py-2 rounded-md hover:opacity-75 active:opacity-50 transition-colors font-cairo ">
+     سجل الدخول
+        </button>)}
+
       </div>
+      {/* Desktop Sign Up Button */}
+      {isLogged ?( <button className="hidden md:block bg-TAF-100 text-white px-4 py-2 rounded-md hover:opacity-75 active:opacity-50 transition-colors font-cairo w-fit whitespace-nowrap ">
+      سجل الخروج
+        </button>):(
+      <button className="hidden md:block bg-TAF-100 text-white px-4 py-2 rounded-md hover:opacity-75 active:opacity-50 transition-colors font-cairo w-fit whitespace-nowrap ">
+      سجل الدخول
+        </button>)}
     </nav>
   );
 }
