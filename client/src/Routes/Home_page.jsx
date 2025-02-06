1ae322dcc8411d517c0a9e148df2a659c79b41bd
Hassanalmaymn <almymnhassan@gmail.com>
2025-02-06 15:28:23 +0300

    last fix and all conficts resolved succefully

--- client/src/Routes/Home_page.jsx
+++ client/src/Routes/Home_page.jsx
@@ -279,23 +279,25 @@ export default function Home_page() {
   } bg-gray-100 shadow-inner shadow-gray-300 rounded-lg w-3/4 p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 scrollbar-thumb-blue-500`;
 
   return (
-    <div className="h-screen w-full bg-gradient-to-b from-TAF-200 via-white to-TAF-200 flex justify-center items-center p-6">
+    <>
       <MainHeader />
-      <div className={course_container}>
-        {courses.map((course) => (
-          <Link>
-            <div
-              key={course.id}
-              className="bg-gradient-to-b from-TAF-200 via-gray-300 to-TAF-300 p-4 rounded-lg shadow-md hover:shadow-lg transition-all"
-            >
-              <h2 className="text-xl font-semibold text-gray-800">
-                {course.title}
-              </h2>
-              <p className="text-gray-600 mt-2">{course.description}</p>
-            </div>
-          </Link>
-        ))}
+      <div className="h-screen w-full bg-gradient-to-b from-TAF-200 via-white to-TAF-200 flex  justify-center items-center p-6">
+        <div className={course_container}>
+          {courses.map((course) => (
+            <Link>
+              <div
+                key={course.id}
+                className="bg-gradient-to-b from-TAF-200 via-gray-300 to-TAF-300 p-4 rounded-lg shadow-md hover:shadow-lg transition-all"
+              >
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {course.title}
+                </h2>
+                <p className="text-gray-600 mt-2">{course.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
