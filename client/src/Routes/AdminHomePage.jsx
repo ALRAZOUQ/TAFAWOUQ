77265a505093443e202645b06885bddddab6f806
Hassanalmaymn <almymnhassan@gmail.com>
2025-03-19 15:34:04 +0300

    view report UI enhancements done need some improvments

--- client/src/Routes/AdminHomePage.jsx
+++ client/src/Routes/AdminHomePage.jsx
@@ -3,7 +3,7 @@ import ReportCard from "../components/ReportCard";
 export default function AdminHomePage() {
   return (
     <div className="min-h-screen max-h-max w-full bg-gradient-to-b from-TAF-200 via-white to-TAF-200">
-      <div className="grid grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mx-4 p-4">
         {reports.map((report) => {
           return (
             <ReportCard
