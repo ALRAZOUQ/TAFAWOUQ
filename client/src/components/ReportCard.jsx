ec865411092fa735001b7013def688e8208939cf
Faisal4z <443102216@student.ksu.edu.sa>
2025-03-22 13:17:43 +0300

    remove comment from comment list after hide it

--- client/src/components/ReportCard.jsx
+++ client/src/components/ReportCard.jsx
@@ -28,7 +28,7 @@ export default function ReportCard({
         </button>
         <button
           className="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-900"
-          onClick={onBanUser}
+          onClick={()=>{}}
         >
           حذف التعليق و حظر المستخدم
         </button>
