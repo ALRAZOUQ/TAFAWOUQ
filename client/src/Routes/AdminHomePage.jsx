2023b7becf56b49b0240d1044316461658633831
Hassanalmaymn <almymnhassan@gmail.com>
2025-03-23 14:05:11 +0300

    now you can reject th e report successfully and it will be deleted from the DB

--- client/src/Routes/AdminHomePage.jsx
+++ client/src/Routes/AdminHomePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "../api/axios";
 import ReportCard from "../components/ReportCard";
+import { toast } from "react-toastify";
 
 export default function AdminHomePage() {
   const [reports, setReports] = useState([]);
@@ -20,6 +21,23 @@ export default function AdminHomePage() {
     fetchReports();
   }, []);
 
+ 
+
+  async function handleReject(reportId) {
+    try {
+      const response = await axios.delete("/admin/deleteReport", {
+        data: { reportId },
+      });
+      if (response.data.success) {
+        setReports(reports.filter((report) => report.reportId !== reportId));
+        toast.success("تم رفض البلاغ بنجاح");
+      }
+    } catch (error) {
+      console.error("Error deleting report:", error);
+      toast.error("حدث خطأ أثناء رفض البلاغ");
+    }
+  }
+
   return (
     <div className="min-h-screen max-h-max w-full bg-gradient-to-b from-TAF-200 via-white to-TAF-200">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 mx-4 p-4">
@@ -29,9 +47,8 @@ export default function AdminHomePage() {
             reason={report.content}
             comment={report.comment.content}
             commentWriter={report.comment.authorName}
-            onBanUser={() => {}}
-            onDeleteComment={() => {}}
-            onReject={() => {}}
+            onReject={() => handleReject(report.reportId)}
+            onDeleteComment={() => handleDeleteReport(report.reportId)}
           />
         ))}
       </div>
