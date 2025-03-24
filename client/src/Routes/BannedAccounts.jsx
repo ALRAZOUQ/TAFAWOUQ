9e9ec4595551c544b93fa664cced3d427e3985b7
Hassanalmaymn <almymnhassan@gmail.com>
2025-03-24 11:47:42 +0300

    fix small problems and add foundation for the work

---
+++ client/src/Routes/BannedAccounts.jsx
@@ -0,0 +1,8 @@
+import Screen from "../components/Screen";
+export default function BsnnedAccounts() {
+  return (
+    <Screen title="BsnnedAccounts">
+      <h1>BsnnedAccounts</h1>
+    </Screen>
+  );
+}
