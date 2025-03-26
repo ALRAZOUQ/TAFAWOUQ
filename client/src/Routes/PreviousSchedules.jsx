6c8de0890f508db7ad259f9742ee77a873bdc9bf
Hassanalmaymn <almymnhassan@gmail.com>
2025-03-26 17:35:36 +0300

    fix some issues some remaining

--- client/src/Routes/PreviousSchedules.jsx
+++ client/src/Routes/PreviousSchedules.jsx
@@ -5,12 +5,13 @@ import axios from "../api/axios";
 export default function PreviousSchedules() {
   const [schedules, setSchedules] = useState([]);
   useEffect(() => {
-    function fetchSchedules() {
+    async function fetchSchedules() {
       try {
-        const response = axios.get("/protected/AllSchedules");
+        const response = await axios.get("/protected/AllSchedules");
         if (response.status === 200) {
           setSchedules(response.data.schedules);
           console.log(response.data.schedules); // TODO: remove this line after testing is done
+          console.log(schedules); // TODO: remove this line after testing is done
         }
       } catch (error) {
         console.log(error);
@@ -21,15 +22,16 @@ export default function PreviousSchedules() {
 
   return (
     <Screen className="flex flex-col gap-4 justify-center items-center">
+        {console.log(schedules.length)}
       {schedules.length > 0 ? (
-        schedules.map((schedule) => {
+        schedules.map((schedule) => (
           <Schedule
             scheduleCourses={schedule.courses}
             createScheduleHandler={() => {}}
             scheduleId={schedule.id}
             key={schedule.id}
-          />;
-        })
+          />
+        ))
       ) : (
         <div className="text-2xl text-red-500">ليس لديك جدول سابق</div>
       )}
