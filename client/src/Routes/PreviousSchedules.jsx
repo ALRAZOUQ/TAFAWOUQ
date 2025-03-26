609d04785da4f34c175ff1438f1448fc9bcbbb72
Hassanalmaymn <almymnhassan@gmail.com>
2025-03-26 16:49:10 +0300

    feat(PreviousSchedules): add data fetching for schedules using useEffect
    Implement data fetching in the PreviousSchedules component to retrieve schedules from the backend API. This ensures the component displays the latest schedules when mounted.

--- client/src/Routes/PreviousSchedules.jsx
+++ client/src/Routes/PreviousSchedules.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Screen from "../components/Screen";
 import Schedule from "../components/Schedule";
+import axios from "../api/axios";
 export default function PreviousSchedules() {
   const [schedules, setSchedules] = useState([]);
+  useEffect(() => {
+    function fetchSchedules() {
+      try {
+        const response = axios.get("/protected/AllSchedules");
+        if (response.status === 200) {
+          setSchedules(response.data.schedules);
+          console.log(response.data.schedules); // TODO: remove this line after testing is done
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchSchedules();
+  }, []);
 
   return (
     <Screen className="flex flex-col gap-4 justify-center items-center">
