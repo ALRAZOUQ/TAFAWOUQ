52909d0c740d95d0e5a9bd09701fc7a51f08254e
Hassanalmaymn <almymnhassan@gmail.com>
2025-04-07 14:46:46 +0300

    chore: update package.json scripts and refactor firebase config
    - Add "start" script to client/package.json for consistency with server
    - Add "dev" script to server/package.json for development clarity
    - Refactor firebase.js for better readability and comment out initialization

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,21 +1,20 @@
-import firebaseAdmin from 'firebase-admin';
-import env from 'dotenv';
-env.config()
-
+import firebaseAdmin from "firebase-admin";
+import env from "dotenv";
+env.config();
 
 const serviceAccount = {
-    "type": process.env.type,
-    "project_id": process.env.project_id,
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": process.env.auth_uri,
-    "token_uri": process.env.token_uri,
-    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-    "client_x509_cert_url": process.env.client_x509_cert_url,
-    "universe_domain": process.env.universe_domain
-}
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
-firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount) })
-export default firebaseAdmin
+//firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount) })
+export default firebaseAdmin;
