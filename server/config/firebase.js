d91686c7b73886862a5f1cef22d98e7e4bad43e3
ALRAZOUQ <rbmale3nk@gmail.com>
2025-04-19 13:44:44 +0300

    try 13: Refactor server>firebase.js

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -2,11 +2,13 @@ import firebaseAdmin from "firebase-admin";
 import env from "dotenv";
 env.config();
 
+const private_key = process.env.private_key?.replace(/\\n/g, "\n");
+
 const serviceAccount = {
     type: process.env.type,
     project_id: process.env.project_id,
     private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key,
+    private_key: private_key,
     client_email: process.env.client_email,
     client_id: process.env.client_id,
     auth_uri: process.env.auth_uri,
@@ -16,10 +18,11 @@ const serviceAccount = {
     universe_domain: process.env.universe_domain,
 };
 
+const noNullsInServiceAccount = Object.values(serviceAccount).every(Boolean)
 /** If you don't have the env variables of the API you will get `false`, If you have it you will get the `firebaseAdmin` 
 */
 let firebaseAdmin_or_false
-if (serviceAccount.type) {
+if (noNullsInServiceAccount) {
     try {
         firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount) })
         firebaseAdmin_or_false = firebaseAdmin
@@ -29,6 +32,8 @@ if (serviceAccount.type) {
         firebaseAdmin_or_false = false
     }
 } else {
+    console.warn("⚠️ Missing Firebase environment variables.");
+
     firebaseAdmin_or_false = false
 }
 
