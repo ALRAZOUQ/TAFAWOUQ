2d6f7423b976595ba55d2743ccf26f57ea8bcd64
Faisal4z <443102216@student.ksu.edu.sa>
2025-04-19 16:17:03 +0300

    add httpOnly to the seassion

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
   })
