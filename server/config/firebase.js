672801a6184f8c8992509631f66d97e03a41909c
ALRAZOUQ <rbmale3nk@gmail.com>
2025-04-19 11:18:22 +0300

    add tryCatch for firebase initializing

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -3,27 +3,33 @@ import env from "dotenv";
 env.config();
 
 const serviceAccount = {
-  type: process.env.type,
-  project_id: process.env.project_id,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url,
-  universe_domain: process.env.universe_domain,
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain,
 };
 
 /** If you don't have the env variables of the API you will get `false`, If you have it you will get the `firebaseAdmin` 
 */
 let firebaseAdmin_or_false
 if (serviceAccount.type) {
-  firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount) })
-  firebaseAdmin_or_false = firebaseAdmin
+    try {
+        firebaseAdmin.initializeApp({ credential: firebaseAdmin.credential.cert(serviceAccount) })
+        firebaseAdmin_or_false = firebaseAdmin
+
+    } catch (error) {
+        console.error(`error while initializing firebaseAdmin 😭 ${error}`)
+        firebaseAdmin_or_false = false
+    }
 } else {
-  firebaseAdmin_or_false = false
+    firebaseAdmin_or_false = false
 }
 
 export default firebaseAdmin_or_false;
