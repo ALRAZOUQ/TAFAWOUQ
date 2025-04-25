87de0c831697565def1c5dc115ec2eddfcd2acbd
ALRAZOUQ <rbmale3nk@gmail.com>
2025-04-25 13:59:23 +0300

    removed the dashbaord-sub-project I plan to use shadcn, I hope I can Configure it

--- client/src/Routes/AdminHomePage.jsx
+++ client/src/Routes/AdminHomePage.jsx
@@ -1,6 +1,6 @@
 import Screen from "../components/Screen";
 import React, { useEffect, useState } from "react";
-import Dashboard from "../Dashboard-Sub-project/pages/Dashboard";
+import Dashboard from "../components/dashboard/Dashboard.jsx";
 export default function AdminHomePage() {
   return (
     <Screen title="Admin Home Page" className="p-2 sm:p-4 md:p-6">
---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { InputOTPDemo } from "./charts/InputOTPDemo";
+
+export default function Dashboard() {
+    return (
+        <div>
+ <InputOTPDemo/>        </div>
+    )
+}
