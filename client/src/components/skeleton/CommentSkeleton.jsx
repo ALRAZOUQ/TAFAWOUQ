e6cd334b51aca5e25cd1c9a8b9d89077e14b272a
Hassanalmaymn <almymnhassan@gmail.com>
2025-05-08 14:17:33 +0300

    fix imports

--- client/src/components/skeleton/CommentSkeleton.jsx
+++ client/src/components/skeleton/CommentSkeleton.jsx
@@ -1,11 +1,12 @@
-import react from "react";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "../ui/skeleton";
 
 export function CommentSkeleton({ isReply = false }) {
   return (
-    <div className={`border border-gray-200 rounded-lg p-4 bg-white shadow-sm space-y-3 mb-4 ${
-      isReply ? "ml-8" : ""
-    }`}>
+    <div
+      className={`border border-gray-200 rounded-lg p-4 bg-white shadow-sm space-y-3 mb-4 ${
+        isReply ? "ml-8" : ""
+      }`}
+    >
       {/* Avatar & data */}
       <div className="flex justify-between items-start">
         <div className="flex items-center gap-2">
@@ -15,7 +16,6 @@ export function CommentSkeleton({ isReply = false }) {
             <Skeleton className="h-3 w-24 bg-slate-300" />
           </div>
         </div>
-       
       </div>
       {/* Comment content */}
       <div className="py-2">
@@ -23,7 +23,6 @@ export function CommentSkeleton({ isReply = false }) {
         <Skeleton className="h-4 w-full bg-slate-300 mb-2" />
         <Skeleton className="h-4 w-11/12 bg-slate-300" />
       </div>
-
     </div>
-  )
+  );
 }
